a0b0acba9870053ed0411e71bf91e37dac76c942
rajansoftnep <rajansoftnep@gmail.com>
2021-05-20 22:18:58 +0545

    iinitial

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Ameilia Hepworth",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81QRyjf28tS._AC_UL200_SR200,200_.jpg",
+    title: "Oh, the Places You'll Go!",
+    author: "Dr. Seuss",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
+    title: "The Vanishing Half: A Novel",
+    author: "Brit Bennett",
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Book from "./Book";
+import { books } from "./books";
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
+}
+
+ReactDom.render(<BookList />, document.getElementById("root"));
